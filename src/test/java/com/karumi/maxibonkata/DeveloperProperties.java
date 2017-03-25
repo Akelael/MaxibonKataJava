7f15f06da71d80604bb60e1bf59224fd00b3c8d4
Akelael <akelael@gmail.com>
2017-03-25 12:17:42 +0100

    Added test to check that there's always at least 2 maxibons in the fridge after a developer grabs her maxibons

--- src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -3,8 +3,10 @@ package com.karumi.maxibonkata;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnitQuickcheck.class) public class DeveloperProperties {
@@ -16,5 +18,11 @@ import static org.junit.Assert.assertTrue;
         assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
     }
 
-
+    @Test public void shouldReturnTheCorrectNumberOfMaxibonsToGrabPerDeveloper() {
+        assertEquals(Karumies.PEDRO.getNumberOfMaxibonsToGrab(), 3);
+        assertEquals(Karumies.JORGE.getNumberOfMaxibonsToGrab(), 1);
+        assertEquals(Karumies.ALBERTO.getNumberOfMaxibonsToGrab(), 1);
+        assertEquals(Karumies.DAVIDE.getNumberOfMaxibonsToGrab(), 0);
+        assertEquals(Karumies.SERGIO.getNumberOfMaxibonsToGrab(), 2);
+    }
 }
